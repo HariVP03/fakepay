a3a60751d8d0ddcc52ca5c7fa9c176ccd8ea1401
HariVP03 <harryskotch11@gmail.com>
2022-05-07 00:07:55 +0530

    android studio works

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'FakePay',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.fakepay",
+  appName: "FakePay",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
